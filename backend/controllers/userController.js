6745a97a68a3498be802a943aaa61c92d9f5e6f9
peteza12345 <pete-e-2012@hotmail.com>
2024-10-20 12:48:57 +0700

    Update User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import Job from '../models/jobModel.js';
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -26,18 +26,16 @@ export const updateUser = async (req, res) => {
   delete newUser.password; // delete password from newUser
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path); // upload image to cloudinary
-    await fs.unlink(req.file.path); // delete image from server
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file); // upload image to cloudinary
 
     newUser.avatar = response.secure_url; // save image url to newUser
     newUser.avatarPublicId = response.public_id; // save image public_id to newUser
   }
-
   const updateUser = await User.findByIdAndUpdate(req.user.userId, newUser);
 
   if (req.file && updateUser.avatarPublicId) { // check if image exists
     await cloudinary.v2.uploader.destroy(updateUser.avatarPublicId); // delete image from cloudinary
   }
-
   res.status(StatusCodes.OK).json({ updateUser, message: 'update user' });
 };
--- backend/middleware/multerMiddleware.js
+++ backend/middleware/multerMiddleware.js
@@ -1,15 +1,17 @@
 import multer from 'multer';
+import DataParser from 'datauri/parser.js';
+import path from 'path';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cd) => {
-    cd(null, 'public/uploads'); // เก็บไฟล์ที่อัพโหลดไว้ที่ public/uploads
-  },
-  filename: (req, file, cd) => {
-    const fileName = file.originalname; // เก็บชื่อไฟล์ที่อัพโหลด
-    cd(null, fileName);
-  }
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage }); // สร้าง multer middleware
+
+const parser = new DataParser();
+
+// สร้าง middleware สําหรับอัพโหลดไฟล์
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString(); // หานามสกุลไฟล์ จากชื่อไฟล์
+  return parser.format(fileExtension, file.buffer).content; // แปลงไฟล์เป็น base64
+};
 
 export default upload;
